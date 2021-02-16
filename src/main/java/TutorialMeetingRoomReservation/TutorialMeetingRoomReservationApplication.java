300a57d9b3832d3bd8286ad61fc086666a0b5da8
yoridori <midorika809+github@gmail.com>
2021-02-17 00:05:56 +0900

    新規追加

---
+++ src/main/java/TutorialMeetingRoomReservation/TutorialMeetingRoomReservationApplication.java
@@ -0,0 +1,13 @@
+package TutorialMeetingRoomReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialMeetingRoomReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialMeetingRoomReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TutorialMeetingRoomReservation/TutorialMeetingRoomReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package TutorialMeetingRoomReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialMeetingRoomReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
